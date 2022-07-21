c8637517a6f55c39d55275a884dacfdbf598684c
onlyhappiness <onlyhappiness96@gmail.com>
2022-07-21 13:54:05 +0900

    [ Update ] user - Generate access token after login

--- controllers/User/signup.js
+++ controllers/User/signup.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
   const user = new User(req.body);
 
   // 회원 가입 시 데이터 저장
-  // TODO: 1. 만약 같은 email이 있으면 같은 이메일이 있다는 에러를 보냄
+  // TODO: 1. 만약 같은 email이 있으면 같은 이메일이 있다는 에러를 보냄 [o]
   // TODO: 2. 비밀번호 암호화 처리
   User.findOne(
     {
