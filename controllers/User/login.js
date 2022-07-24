147e254e766d86f4a141a520aed48e3e9055983b
onlyhappiness <onlyhappiness96@gmail.com>
2022-07-24 22:32:12 +0900

    [ Update ] login - 암호화 password 비교

--- controllers/User/login.js
+++ controllers/User/login.js
@@ -1,20 +1,27 @@
-const { unknownError } = require("../../error/errorcode");
+const { unknownError, noMatchUser } = require("../../error/errorcode");
 const { generateAccessToken } = require("../../middlewares/jwt");
 const { User } = require("../../models/user");
+const crypto = require("crypto-js");
 
 module.exports = async (req, res, next) => {
   const { email, password } = req.body;
+
+  const hash = crypto.SHA256(password, process.env.SALT).toString();
 
   // 조건에 해당하는 첫번째 것을 쿼리
   User.findOne(
     {
       email: email,
-      password: password,
+      password: hash,
     },
     (err, userData) => {
       if (err) {
         console.log(err);
         return next(unknownError);
+      }
+
+      if (!userData) {
+        return next(noMatchUser);
       }
 
       const accessToken = generateAccessToken(userData?._id.toJSON());
