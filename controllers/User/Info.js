b740ee960e37f6fa341d8eea217ea60e951abc1f
onlyhappiness <onlyhappiness96@gmail.com>
2022-07-26 14:31:31 +0900

    [ Update ] 유저 정보 요청

--- controllers/User/Info.js
+++ controllers/User/Info.js
@@ -1,6 +1,23 @@
+const { User } = require("../../models/user");
+
+const { isAuthorized } = require("../../middlewares/jwt");
+const { unknownError, notFoundUser } = require("../../error/errorcode");
+
 module.exports = async (req, res, next) => {
-  return res.status(200).send({
-    message: "유저 정보 요청",
-    data: null,
+  const tokenVerify = isAuthorized(req);
+
+  User.findById(tokenVerify, (err, info) => {
+    if (err) {
+      return next(unknownError);
+    }
+
+    if (!info) {
+      return next(notFoundUser);
+    }
+
+    return res.status(200).send({
+      message: "유저 정보 요청",
+      data: info,
+    });
   });
 };
--- error/errorcode.js
+++ error/errorcode.js
@@ -1,10 +1,15 @@
 module.exports = {
-  // 401
   noMatchUser: {
-    status: 401,
+    status: 400,
     message: "아이디 또는 비밀번호를 확인해주세요",
     errorcode: "401",
   },
+  ExistingUsers: {
+    status: 400,
+    message: "이미 존재하는 유저 이메일이 있습니다.",
+    errorCode: "400",
+  },
+  // 401
   invalidToken: {
     status: 401,
     message: "올바르지 않은 토큰입니다.",
@@ -15,10 +20,12 @@ module.exports = {
     message: "만료된 토큰입니다.",
     errorCode: "401",
   },
-  ExistingUsers: {
-    status: 401,
-    message: "이미 존재하는 유저 이메일이 있습니다.",
-    errorCode: "401",
+
+  // 404
+  notFoundUser: {
+    status: 404,
+    message: "요청하신 유저가 존재하지 않습니다.",
+    errorCode: "404",
   },
 
   // 500
