600f671387d8052174009c9ec51bd77d8d91b317
onlyhappiness <onlyhappiness96@gmail.com>
2023-04-11 22:26:07 +0900

    Feat: S3 이미지 업로드 작업 중..

--- src/upload/controller/upload.controller.ts
+++ src/upload/controller/upload.controller.ts
@@ -6,13 +6,17 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
+import { UploadService } from '../service/upload.service';
 
 @ApiTags('UPLOAD')
 @Controller('upload')
 export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() image) {
+    console.log(image);
+    return await this.uploadService.uploadFile(image);
   }
 }
--- src/upload/service/upload.service.ts
+++ src/upload/service/upload.service.ts
@@ -5,28 +5,28 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UploadService {
   private readonly config = {
-    region: '',
+    region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId: '',
-      secretAccessKey: '',
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
     },
   };
 
   private readonly s3 = new S3(this.config);
 
-  async uploadFile(image: Express.Multer.File) {
+  async uploadFile(image: any) {
     try {
       const key = uuidv4();
       const response = await this.s3
         .upload({
-          Bucket: '',
-          Key: '',
-          Body: '',
-          ContentType: '',
+          Bucket: process.env.AWS_S3_BUCKET,
+          Key: key,
+          Body: image.buffer,
+          ContentType: 'image/jpg',
         })
         .promise();
 
-      return response;
+      return response?.Location;
     } catch (err) {
       console.log(err);
       throw new InternalServerErrorException('S3 업로드 실패');
