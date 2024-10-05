7977d86749d4224e5f8e9d6c651a5941cdfc16b8
onlyhappiness <onlyhappiness96@gmail.com>
2024-10-05 23:16:57 +0900

    remove: dev

--- src/auth/dto/user.login.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { Users } from 'src/user/model/user.entity';
-
-export class UserLoginDTO extends PickType(Users, [
-  'signname',
-  'password',
-] as const) {}
